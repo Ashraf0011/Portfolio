f5396a486463102fa490ef3cbd7c242aed23676e
Ash0011 <Mycdu.edu@icloud.com>
2020-08-09 15:30:09 +0930

    Updates

---
+++ js/collatz.js
@@ -0,0 +1,79 @@
+const leng = 40;
+const weigth = 3;
+const scale = 0.3;
+const angle = 0.2;
+const lineWeightCoff = 0.01;
+const startHeading = -90;
+let s = 1;
+let inc = 500;
+let startPos;
+
+let canvas;
+
+function setup() {
+    canvas = createCanvas(window.innerWidth, window.innerHeight);
+    translate(window.innerWidth / 2, window.innerHeight);
+    stroke("#fcb69f");
+    noFill();
+    strokeWeight(weigth * scale);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-3');
+
+    //   background("#F2F2F2");
+}
+
+
+function draw() {
+    startPos = createVector(window.innerWidth / 2, window.innerHeight);
+
+    let sequence = [];
+    let n = s;
+
+    do {
+        sequence.push(n);
+        n = Collatz(n);
+    } while (n != 1);
+    sequence.push(1);
+    sequence.reverse();
+
+    let pos = startPos.copy();
+    let h = startHeading;
+    let lWcoff = 1;
+
+    for (let j = 0; j < sequence.length; j++) {
+        let value = sequence[j];
+        if (value % 2 == 0) {
+            h += angle;
+        } else {
+            h -= angle;
+        } //Eif angle
+        strokeWeight(weigth * lWcoff);
+        lWcoff -= lineWeightCoff;
+        beginShape();
+        vertex(pos.x, pos.y);
+        pos.add(createVector(leng * scale, 0).rotate(h));
+        vertex(pos.x, pos.y);
+        endShape();
+    } //Efor lines
+    s++;
+    if (s > 5000) {
+        s = 1;
+    }
+
+
+} //Edraw
+
+
+function Collatz(n) {
+    if (n % 2 == 0) {
+        //even 
+        return n / 2;
+    } else {
+        //odd
+        return (n * 3 + 1) / 2;
+    }
+}
+
+function windowResized() {
+    resizeCanvas(window.innerWidth, window.innerHeight);
+}
